bec2e728cb12f477e4f5ea5bbb2905dec0631d2d
accessgames <83598795+accessgames@users.noreply.github.com>
2024-01-16 15:43:41 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
 const { app, browserWindow } = require('electron');
+import * as path from "path"
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      nativeWindowOpen: true,
+    }
+  })
+
+  win.loadFile(path.join(__dirname, "main.html"))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
